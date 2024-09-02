75360f43469625cb864dc22b58c3f60737e92477
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-09-02 19:38:03 -0400

    le puse la T a Tecnicos

--- RegistroTecnicos/DAL/Contexto.cs
+++ RegistroTecnicos/DAL/Contexto.cs
@@ -7,5 +7,5 @@ public class Contexto:DbContext
 {
     public Contexto(DbContextOptions<Contexto>options)
     :base(options){ }
-    public DbSet<Tecnicos> tecnicos { get; set; }
+    public DbSet<Tecnicos> Tecnicos { get; set; }
 }
