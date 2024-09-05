68548e075b2a81db6c720575583c0c166d962b40
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-09-04 21:59:51 -0400

    le agregue el espacio

--- RegistroTecnicos/Models/TiposTecnicos.cs
+++ RegistroTecnicos/Models/TiposTecnicos.cs
@@ -7,6 +7,5 @@ public class TiposTecnicos
     public int TipoId { get; set; }
     [Required(ErrorMessage = "Por favor, asegúrate de que llenes campo")]
     public string? Descripcion { get; set; }
-
     public Tecnicos? Tecnicos { get; set; }
 }
