15bfb7e5c8f32169d93d790037bdf1db6d4b2a4d
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-09-04 21:46:45 -0400

    El modelo de TiposTecnicos y le agregue la llave foreign key

--- RegistroTecnicos/Models/TiposTecnicos.cs
+++ RegistroTecnicos/Models/TiposTecnicos.cs
@@ -7,4 +7,6 @@ public class TiposTecnicos
     public int TipoId { get; set; }
     [Required(ErrorMessage = "Por favor, asegúrate de que llenes campo")]
     public string? Descripcion { get; set; }
+
+    public Tecnicos? Tecnicos { get; set; }
 }
