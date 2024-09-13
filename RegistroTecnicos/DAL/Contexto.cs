82023a0d421e0e17d939ee2e07d8a8c46aa51a55
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-09-13 08:15:04 -0400

    El contexto del trabajos le arregle el espacio yn namespace

--- RegistroTecnicos/DAL/Contexto.cs
+++ RegistroTecnicos/DAL/Contexto.cs
@@ -1,16 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RegistroTecnicos.Models;
 
-namespace RegistroTecnicos.DAL
+namespace RegistroTecnicos.DAL;
+
+public class Contexto : DbContext
 {
-    public class Contexto : DbContext
-    {
-        public Contexto(DbContextOptions<Contexto> options)
-            : base(options) { }
-        public DbSet<Tecnicos> Tecnicos { get; set; }
-        public DbSet<TiposTecnicos> TiposTecnicos { get; set; }
-        public DbSet<Clientes> Clientes { get; set; }
-        public DbSet<Trabajos> Trabajos { get; set; }
-    }
+    public Contexto(DbContextOptions<Contexto> options)
+        : base(options) { }
+    public DbSet<Tecnicos> Tecnicos { get; set; }
+    public DbSet<TiposTecnicos> TiposTecnicos { get; set; }
+    public DbSet<Clientes> Clientes { get; set; }
+    public DbSet<Trabajos> Trabajos { get; set; }
 }
 
