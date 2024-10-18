90472682123a1088ebee43b250d45f80df027b1b
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-10-18 09:57:19 -0400

    El Contexto Articulos Con ModelCreating

--- RegistroTecnicos/DAL/Contexto.cs
+++ RegistroTecnicos/DAL/Contexto.cs
@@ -10,7 +10,6 @@ public class Contexto : DbContext
     public DbSet<TiposTecnicos> TiposTecnicos { get; set; }
     public DbSet<Clientes> Clientes { get; set; }
     public DbSet<Trabajos> Trabajos { get; set; }
-
     public DbSet<Prioridades> Prioridades { get; set; }
     public DbSet<Articulos> Articulos { get; set; }
     public DbSet<TrabajosDetalle> TrabajosDetalle { get; set; }
