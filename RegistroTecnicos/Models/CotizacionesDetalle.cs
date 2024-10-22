75975316a48154e68aa8ff4ba8bdf91313d93830
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-10-22 13:31:33 -0400

    El Modelo CotizacionesDetalle

---
+++ RegistroTecnicos/Models/CotizacionesDetalle.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicos.Models;
+
+public class CotizacionesDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+
+    [ForeignKey("CotizacionId")]
+    [Required(ErrorMessage = "La CotizacionId es obligatorio.")]
+    public int CotizacionId { get; set;}
+    public Cotizaciones? Cotizaciones { get; set; }
+
+    [ForeignKey("ArticuloId")]
+    [Required(ErrorMessage = "El ArticuloId es obligatorio.")]
+    public int ArticuloId { get; set; }
+
+    public Articulos? Articulos { get; set; }
+    [Range(0.01, double.MaxValue, ErrorMessage = "La cantidad debe ser mayor que 0.")]
+    public decimal? Cantidad { get; set; }
+    [Range(0.01, double.MaxValue, ErrorMessage = "El Precio debe ser mayor que 0.")]
+    public double? Precio { get; set; }
+}
