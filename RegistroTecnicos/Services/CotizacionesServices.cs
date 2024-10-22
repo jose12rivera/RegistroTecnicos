43b8b1c02174077c126a93d92d760b2d66230bec
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-10-22 14:46:40 -0400

    El services de Cotizaciones

---
+++ RegistroTecnicos/Services/CotizacionesServices.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnicos.DAL;
+using RegistroTecnicos.Models;
+using System.Linq.Expressions;
+
+namespace RegistroTecnicos.Services;
+
+public class CotizacionesServices
+{
+    private readonly Contexto _contexto;
+    public CotizacionesServices(Contexto contexto)
+    {
+        _contexto = contexto;
+    }
+    public async Task <bool>Existe(int CotizacionId)
+    {
+        return await _contexto.Cotizaciones.AnyAsync(c=>c.CotizacionId == CotizacionId);
+
+    }
+    private async Task <bool> Insertar(Cotizaciones cotizacion)
+    {
+        _contexto.Cotizaciones.Add(cotizacion);
+        return await _contexto.SaveChangesAsync() > 0;
+    }
+    private async Task<bool> Modificar (Cotizaciones cotizacion)
+    {
+        _contexto.Cotizaciones.Update(cotizacion);
+        return await _contexto.SaveChangesAsync() > 0;
+    }
+    public async Task <bool> Guardar(Cotizaciones cotizacion)
+    {
+        if(!await Existe(cotizacion.CotizacionId)) 
+            return await Insertar(cotizacion);
+        else
+            return await Modificar(cotizacion);
+
+    }
+    public async Task <bool>Eliminar(int id)
+    {
+        var eliminado = await _contexto.Cotizaciones
+            .Where(c => c.CotizacionId == id)
+            .ExecuteDeleteAsync();
+        return eliminado > 0;
+    }
+    public async Task <Cotizaciones?>Buscar(int id)
+    {
+        return await _contexto.Cotizaciones
+            .Include(c => c.Clientes)
+            .Include(c => c.CotizacionesDetalle)
+             .Include(c=>c.Articulos)
+            .AsNoTracking()
+            .FirstOrDefaultAsync();
+    }
+    public async Task <List<Cotizaciones>>Listar(Expression<Func<Cotizaciones, bool>> Criterio)
+    {
+        return await _contexto.Cotizaciones
+            .Include (c => c.Clientes)
+            .Include(c=>c.CotizacionesDetalle)
+            .Include(c => c.Articulos)
+            .AsNoTracking()
+            .Where(Criterio)
+            .ToListAsync();
+    }
+    public async Task<List<Cotizaciones>> ListaCotizaciones()
+    {
+        return await _contexto.Cotizaciones
+           .AsNoTracking()
+           .ToListAsync();
+    }
+
+}
