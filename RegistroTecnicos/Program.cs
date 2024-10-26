8977a2d03ab3c07fa90a588387e8a9fd445c5851
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2024-10-26 16:16:21 -0400

    El program

--- RegistroTecnicos/Program.cs
+++ RegistroTecnicos/Program.cs
@@ -4,56 +4,53 @@ using RegistroTecnicos.DAL;
 using RegistroTecnicos.Models;
 using RegistroTecnicos.Services;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-//La inyeccion Del Contexto Con SqlServer
-var SqlConStr = builder.Configuration.GetConnectionString("SqlConStr");
-builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlServer(SqlConStr));
-
-//La inyeccion Del services
-builder.Services.AddScoped<TecnicosServices>();
-
-//La inyeccion del services Tipos Tecnicos
-builder.Services.AddScoped<TiposTecnicosServices>();
-
-//La inyeccion del services Clientes
-builder.Services.AddScoped<ClientesServices>();
-
-//La inyeccion del services Trabajos
-builder.Services.AddScoped<TrabajosServices>();
-
-//La inyeccion del services de  Prioridades
-builder.Services.AddScoped<PrioridadesServices>();
-
-//La inyeccion del services de  Articulos
-builder.Services.AddScoped<ArticulosServices>();
-
-//La inyeccion del services de  CotizacionesServices
-builder.Services.AddScoped<CotizacionesServices>();
-//La inyeccion del Bootstrap
-builder.Services.AddBlazorBootstrap();
-
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+namespace RegistroTecnicos
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            // La inyección del contexto con SqlServer
+            var SqlConStr = builder.Configuration.GetConnectionString("SqlConStr");
+            builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlServer(SqlConStr));
+
+
+            // La inyección de servicios adicionales especificados en el segundo código
+            builder.Services.AddScoped<TecnicosServices>();
+            builder.Services.AddScoped<TiposTecnicosServices>();
+            builder.Services.AddScoped<ClientesServices>();
+            builder.Services.AddScoped<TrabajosServices>();
+            builder.Services.AddScoped<PrioridadesServices>();
+            builder.Services.AddScoped<ArticulosServices>();
+            builder.Services.AddScoped<CotizacionesServices>();
+
+            // La inyección del Bootstrap
+            builder.Services.AddBlazorBootstrap();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error", createScopeForErrors: true);
+                // The default HSTS value is 30 días. Puedes cambiar esto para escenarios de producción si es necesario.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
-
-app.Run();
